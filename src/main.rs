1e1f340d86ae6873874555816a0a958a1f4d8d7b
ArvinNotDev <arveenjafari19@gmail.com>
2025-02-06 12:33:08 +0000

    rust practice updated!

--- src/main.rs
+++ src/main.rs
@@ -40,8 +40,8 @@
 //     // functions
 
 //     // functions can be inside or outside of the main function
-//     // we must say that the what will come to the function 
-//     // for example in here we want to pass a string so we will
+//     // we must say that what types of the variables will come to the function 
+//     // for example in this case we want to pass a string so we will
 //     // mention that the type of name is &str
 
 //     fn printHello(name: &str){
@@ -50,7 +50,7 @@
 //     printHello("arvin");
     
     
-//     // we can mention that what we are going to return from function
+//     // we can mention what we will return from function
 //     // for example:
 //     fn sum(num1: i32, num2: i32) -> i32 {
 //         num1 + num2 // now i dont have to place a ";" cause if i do
@@ -70,7 +70,7 @@
 //     let result = returning_tuple(1, 2, 3);
 //     println!("{:?}", result); // you cannot print a tuple or array or vector like normal
 //                               // remember this syntax, it will format them to something
-//                               // it can print! so so so unique syntax :)
+//                               // it can print! what a unique syntax :)
 
     
 
@@ -85,7 +85,8 @@
 //         println!("{}", format!("{a} {s}"));
 //         // we cannot access these variables outside of the code Block! ;)
 //         // code Blocks can return values too! so Coooooool...
-//         // we cannot reuse code Blocks!
+//         // * => we cannot reuse code Blocks!
+//         // so code Blocks and functions are the same but in code Blocks you cannot do *
 //     }
 
 
@@ -115,16 +116,15 @@
 //     // intresting syntax for one line if else variables!
 //     let num = 80;
 //     let grade = if num >= 90 {
-//         'A' // do not place ';'
-//     } else if num >=60 {
-//         'B' // do not place ';'
+//         'A'                     // do not place ';'
+//     } else if num >= 60 {
+//         'B'                     // do not place ';'
 //     } else {
-//         'C' // do not place ';'
+//         'C'                     // do not place ';'
 //     };
 
 
 
-    
 //     // using match for the same case!
 //     let mut grade = 'A';
 //     match num{
@@ -151,9 +151,9 @@
 //     loop {
 //         println!("im in the loop");
 //         break;
-//     } // but you can go out of it by break
+//     } // its an infinite loop but you can go out of it by break
 
-//     // we can Label something like:
+//     // we can Label(naming after it) something like:
 //     'label: loop {
 //         println!("now im in a loop which has label");
 
@@ -162,7 +162,7 @@
 //     }
 
 
-//     // we can make a var of value with loops
+//     // we can fill a defined variable using loops
 //     // for example:
 //     let mut b = 1;
 //     let a = loop{
@@ -173,9 +173,8 @@
 //             break b;
 //         }
 //     };
-//     // now you can see that in this case when b reached 5, a value became 5!
-//     // OUTPUT: a=5
-//     println!("a={}", a);
+//     // now you can see that in this case when b reached 5, "a" value became 5!
+//     println!("a={}", a); // OUTPUT: a=5
 
 
 
@@ -209,19 +208,18 @@
 
 
 
-//     // if we make a var and not use it it gives us warning, lets fix it:
+//     // if we define a variable but do not using it, compiler gives us warning, lets fix it:
 //     // let mamamia = "hi";
 //     // so we will use something like this
 //     let _mamamia = "hi";
-//     print!("{_mamamia}");
+//     // now the warning is gone!
 
 
 
-
-//     // static is something like a const but...
+//     // static is something like a constant variables but...
 //     // if you want to store tons of data in a var
 //     // static is a better choice!
-//     static data: &str = "this is my data";
+//     static data: &str = "this is my data and i wont change it...";
 
 
 
@@ -229,21 +227,133 @@
 
 
 
-
-
-
-
-
-
-
-
 // _______________________________________________part-2___________________________________________________
 
-fn main(){
+// fn main(){
 
-    // look at this great example of ref value
-    let n = String::from("22");
-    let b = &n; // here we can use both n.clone() or &n
-    println!("n is {} and b is {}", n, b)
+//     // look at this great example of ref value
+//     let n = String::from("22");
+//     let b = &n; // here we can use both n.clone() or &n
+//     println!("n is {} and b is {}", n, b)
 
+// }
+// __________________________________________________________________
+
+
+
+
+
+
+// xX Object oriented programming using rust Xx
+
+
+// in this case we are using "f64" type and remember that you cannot use refrence types like &str
+// with same syntax cause then you have to make sure to specify a lifetime like this <'a> 
+// if you want to use string its better to use "String" instead of %str, String is an owned type :) 
+
+
+// example with f64 for a BankAccount class:
+
+// pub struct BankAccount { // in rust we are making instances using structs
+//     balance: f64, // Private field
+// }
+
+// impl BankAccount {
+//     // Public constructor, same as def __init__(): in python
+//     pub fn new(initial_balance: f64) -> Self {
+//         BankAccount { balance: initial_balance }
+//     }
+
+//     // Public method to deposit money
+//     pub fn deposit(&mut self, amount: f64) {
+//         self.balance += amount;
+//     }
+
+//     // Public method to get balance
+//     pub fn get_balance(&self) -> f64 {
+//         self.balance
+//     }
+// }
+
+// fn main() {
+//     let mut account = BankAccount::new(100.0);
+//     account.deposit(50.0);
+//     println!("Balance: {}", account.get_balance()); // Output: Balance: 150.0
+// }
+
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\
+
+
+// Example with String for Person class:
+
+// pub struct Person {
+//     first_name: String,
+//     last_name: String,
+//     age: i32
+// }
+
+// impl Person {
+    
+//     pub fn new(initial_first_name: String, initial_last_name: String, initial_age: i32) -> Self{
+//         Person {first_name: initial_first_name, last_name:initial_last_name, age: initial_age}
+//     }
+
+//     pub fn get_full_name(&self) -> String{
+//         format!("{} {}", self.first_name, self.last_name)
+//     }
+
+//     pub fn get_age(&self) -> i32 {
+//         self.age
+//     }
+// }
+
+// fn main(){
+//     let p1 = Person{first_name: String::from("Arvin"), last_name: String::from("Jafari"), age: 20};
+//     println!("{}", p1.get_full_name());
+//     println!("Age: {}", p1.get_age())
+// }
+
+
+
+
+// In this case, we are using `&str` (string slices) instead of `String`.
+// Since `&str` is a reference type, we need to specify lifetimes to ensure the references are valid.
+
+// Define the `Person` struct with a lifetime parameter `'a`.
+// This tells the compiler that the references (`first_name` and `last_name`) must live at least as long as the struct.
+pub struct Person<'a> {
+    first_name: &'a str, // Reference type so we need to set a lifetime
+    last_name: &'a str,  // Reference type so we need to set a lifetime
+    age: i32,            // Owned type, no lifetime needed
+}
+
+// The lifetime `'a` must be specified here as well to match the struct definition.
+impl<'a> Person<'a> {
+
+    pub fn new(initial_first_name: &'a str, initial_last_name: &'a str, initial_age: i32) -> Self {
+        Person {
+            first_name: initial_first_name,
+            last_name: initial_last_name,
+            age: initial_age,
+        }
+    }
+
+    // now after initialing them thats same as before...
+
+    pub fn get_full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+
+    pub fn get_age(&self) -> i32 {
+        self.age
+    }
+}
+
+fn main() {
+    // Create a `Person` instance using string literals (which have the `'static` lifetime).
+    let p1 = Person::new("Arvin", "Jafari", 20);
+
+    // Print the full name and age.
+    println!("Full Name: {}", p1.get_full_name()); // Output: Full Name: Arvin Jafari
+    println!("Age: {}", p1.get_age());            // Output: Age: 20
 }
